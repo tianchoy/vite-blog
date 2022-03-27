e40092064773a280153ff1169f971ee4a48b4a28
tianchoy <tianchoy@live.com>
2022-03-27 12:24:15 +0800

    Delete apis directory

--- apis/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sit_link="localhost";
-$user="root"; //数据库名称
-$pw="root";   //数据库密码
-$sql="blog";
-@$link=mysqli_connect($sit_link,$user,$pw) or die ("数据库连接失败");
-$blog=mysqli_select_db($link,$sql) or die ("选择表失败");
-?>
